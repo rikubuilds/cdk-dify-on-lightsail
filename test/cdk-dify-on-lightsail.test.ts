9ddc953a8855beeda649e1528507fe17f0ad6840
rikrikurik <55145573+rikrikurik@users.noreply.github.com>
2024-05-05 20:36:11 +0900

    Initial commit

---
+++ test/cdk-dify-on-lightsail.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDifyOnLightsail from '../lib/cdk-dify-on-lightsail-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-dify-on-lightsail-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDifyOnLightsail.CdkDifyOnLightsailStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
